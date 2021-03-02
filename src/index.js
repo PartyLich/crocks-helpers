aaba9006811ad7355bfc671a57ece9a722bcdb04
Arthur E. Jones <PartyLich@gmail.com>
2021-03-02 14:07:31 -0600

    fix: correct root exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+// @flow
 import * as result from './result';
 import ensure from './ensure';
-export { ensure };
 
-export default Object.assign(
-    {},
-    { ensure },
-    result,
-);
+export { ensure };
+export * from './result';
+
+export default {
+  ensure,
+  ...result,
+};
